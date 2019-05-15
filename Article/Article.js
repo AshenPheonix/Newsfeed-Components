1ff8369502cf7925d3512289006b1a265691b571
Brandon Porter <brendlorend@gmail.com>
2019-05-15 10:27:11 -0700

    Stretch: Some done

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,5 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
+import dataSet from '../DBS/articles.js'
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
@@ -9,14 +9,39 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent='Expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click',(e)=>{
-      this.expandArticle()
-    })
+    this.expandButton.addEventListener('click',(e)=>this.expandArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open')
+    this.expandButton.textContent=(this.expandButton.textContent==='Expand'?'Close':'Expand')
+  }
+}
+
+//Stretch
+class ArticleBuilder{
+  constructor(artData){
+    this.root=document.createElement('div')
+    this.head=document.createElement('h2')
+    this.date=document.createElement('p')
+    this.button=document.createElement('span')
+
+    this.root.classList.add('article')
+    this.date.classList.add('date')
+    this.button.classList.add('expandButton')
+
+    this.head.textContent=artData.head
+    this.date.textContent=artData.date
+    this.root.appendChild(this.head)
+    this.root.appendChild(this.date)
+    artData.art.forEach(item=>{
+      let temp=document.createElement('p')
+      temp.textContent=item
+      this.root.appendChild(temp)
+    })
+    this.root.appendChild(this.button)
+    document.querySelector('.articles').appendChild(this.root)
   }
 }
 
@@ -28,8 +53,11 @@ class Article {
 
 */
 
+let articleRemote=new ArticleBuilder(dataSet[0])
+
 let articles = document.querySelectorAll('.article');
 articles.forEach(item=>{
   console.log(item);
   new Article(item)
 })
+
